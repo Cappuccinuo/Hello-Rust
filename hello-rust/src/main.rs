fdbe5686adf0b8186b7113873cb9d5bdc00439df
Cappuccinuo <1006901532@qq.com>
2020-12-01 20:59:22 +0800

    Follow the getting started page and create the initial hello-rust program

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,18 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+// Ref: https://www.rust-lang.org/learn/get-started
+
+// Had to run following during `cargo build`
+// rm -rf ~/.cargo/registry/index/*
+// rm -rf ~/.cargo/.package-cache
+// https://stackoverflow.com/questions/47565203/cargo-build-hangs-with-blocking-waiting-for-file-lock-on-the-registry-index-a?rq=1
+// to fix Blocking waiting for file lock on package cache
+fn main() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans");
+    let width = message.chars().count();
+    
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
+}
